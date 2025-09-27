04ee0bf65e43284589f13e6eec0521f40e214333
aldocesarmg <aldocesarmg@gmail.com>
2025-09-26 19:46:44 -0600

    Rolodex finished with core modules

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-import { writeFileSync } from "fs"; // to interact with your computer's file system (create, read, write, update, delete, etc)
+import { writeFileSync, appendFileSync, existsSync, write } from "fs"; // to interact with your computer's file system (create, read, write, update, delete, etc)
 import { createInterface } from "readline"; // to read data from a readable stream one line at a time
 import { promisify } from 'util'; // to convert callback-based 
-
-/* file creation and population
-const testContent = "testing...";
-
-try {
-    writeFileSync("./test.txt", testContent);
-    console.log("success");
-} catch (err) {
-    console.log(err);
-}
-
-*/
 
 const readline = createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function getAnswer(message) {
-    return new Promise((resolve) => {
-        readline.question(message, (answer) => {
-            resolve(answer);
-            readline.close();
-        })
-    })
-}
+class Person {
+    constructor(name = "", number = "", email = "") {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
 
-// getAnswer('tell me');
+    saveToCSV() {
+        if (!existsSync("./contacts.csv")) {
+            writeFileSync("./contacts.csv", "");
+        }
 
-const questionPromisified = promisify(readline.question).bind(readline); // we need to use bind() function in order to replace global this (which englobes all variables of the program) to this (the actual values from the function)
-
-async function getAnswerWithPromisify() {
-    try {
-        const answer = await questionPromisified('tell me something\n');
-        console.log(`This is your input: ${answer}`);
-    } catch (e) {
-        console.log(e);
-    } finally {
-        readline.close();
+        const content = `${this.name},${this.number},${this.email}\n`;
+        try {
+            appendFileSync("./contacts.csv", content);
+            console.log(`${this.name}: Contact saved!`)
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
 
-getAnswerWithPromisify();
+async function getAnswerWithPromisify(message) {
+    try {
+        const questionPromisified = promisify(readline.question).bind(readline); // we need to use bind() function in order to replace global this (which englobes all variables of the program) to this (the actual values from the function)
+        return await questionPromisified(message);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function startApp() {
+    let shouldContinue = true;
+
+    do {
+        const name = await getAnswerWithPromisify('Enter your name: ');
+        const number = await getAnswerWithPromisify('Enter your number: ');
+        const email = await getAnswerWithPromisify('Enter your email: ');
+
+        const newPerson = new Person(name, number, email);
+        newPerson.saveToCSV();
+
+        const response = await getAnswerWithPromisify('Should we continue adding contacts (Y/N)?');
+        if (response.toLowerCase() === "n") shouldContinue = false;
+    } while (shouldContinue);
+}
+
+try {
+    await startApp();
+} catch(e) {
+    console.error(e);
+} finally {
+    readline.close();
+}
